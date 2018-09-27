56590a1414783d1763e42fe0ceea82fd7d1462a6
Saransh Sharma <saransh1337@gmail.com>
2018-09-27 15:14:33 +0530

    add logging wrapper class

---
+++ app/src/androidTest/java/com/example/saransh1337/droidappcoldstarttime/Logging_ColdStart.java
@@ -0,0 +1,33 @@
+package com.example.saransh1337.droidappcoldstarttime;
+
+import android.util.Log;
+
+public class Logging_ColdStart {
+
+
+    public Logging_ColdStart() {
+
+    }
+
+    public static void verbose(String logStr) {
+        Log.v(ConfigColdStartTest.coldStartTag, logStr);
+    }
+
+    public static void trace(String logStr) {
+        Log.d(ConfigColdStartTest.coldStartTag, logStr);
+    }
+
+    public static void debug(String logStr) {
+        Log.d(ConfigColdStartTest.coldStartTag, logStr);
+    }
+
+    public static void info(String logStr) {
+        Log.i(ConfigColdStartTest.coldStartTag, logStr);
+    }
+
+    public static void warn(String logStr) {
+        Log.w(ConfigColdStartTest.coldStartTag, logStr);
+    }
+
+
+}
