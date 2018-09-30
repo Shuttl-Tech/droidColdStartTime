a8f1235d317d2ee4ff613c4ef78f1c4ca1041f6c
Saransh Sharma <saransh1337@gmail.com>
2018-10-01 00:31:19 +0530

    add debug, release packages to config

--- app/src/androidTest/java/com/example/saransh1337/droidappcoldstarttime/ConfigColdStartTest.java
+++ app/src/androidTest/java/com/example/saransh1337/droidappcoldstarttime/ConfigColdStartTest.java
@@ -2,7 +2,9 @@ package com.example.saransh1337.droidappcoldstarttime;
 
 public class ConfigColdStartTest {
 
-    public static String PACKAGENAME_CONSUMER = "app.goplus.in.myapplication";
+    public static String PACKAGENAME_CONSUMER_RELEASE = "app.goplus.in.myapplication";
+    public static String PACKAGENAME_CONSUMER_DEBUG = "app.goplus.in.myapplication.debug";
+
     public static String coldStartTag = "droidColdStart";
 
 }
